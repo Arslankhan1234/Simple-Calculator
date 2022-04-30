77e871f9bbdf09f8b9c3a4ec95da07a823bb3f38
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-04-29 23:02:36 -0700

    Different buttons with id

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+ float var1;
+ float var2;
+ String opt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
